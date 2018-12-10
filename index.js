aad3e7f9a76fdd19b3456398014d24d81032acae
Anh Thuy Le <anhthuyle@Anhs-iMac.local>
2018-12-09 19:27:19 -0800

    to-do-list-all-calls

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const Task = require('./taskSchema.js');
 const app = express();
-const PORT = 9001;
+const PORT = 1337;
 
 mongoDB = 'mongodb://localhost/todo-list';
 mongoose.connect(mongoDB);
@@ -18,10 +18,25 @@ db.on('open', () => console.log('MongoDB successfully connected'));
 // app.get('/', (req, res) => res.send('Hello This is working'));
 app.get('/task/:id', (req, res) => {
   //TODO: Given a task id, return the task to client
+  const id = req.params.id;
+  Task.findById (id).then((task) => {
+    if (!task) {
+      return res.send('Task does not exist');
+    }
+    return res.send(task);
+  }).catch((err) => {
+    return res.status(500).send(err);
+  })
 });
 
 app.get('/task', (req, res) => {
   //TODO: Return all tasks to client
+  Task.find ({}).then( (taskList) => {
+    if (taskList.length < 1) {
+      return res.send('Tasks list is empty');
+    }
+    return res.send(taskList);
+  })
 });
 
 app.post('/task', (req, res) => {
@@ -45,13 +60,32 @@ app.post('/task', (req, res) => {
 
 app.put('/task/:id', (req, res) => {
   // TODO: Edit a task based on an ID;
+  const id = req.params.id;
+  const { title, description } = req.body;
+  if (!title || !description) {
+    return res.send('Error! Please provide title and description');
+  }
+  Task.findByIdAndUpdate (id, {title, description}).then((task) => {
+    return res.send(task);
+  })
 });
 
 app.delete('/task/:id', (req, res) => {
   // TODO: remove a task based on an ID;
+  const id = req.params.id;
+  Task.findByIdAndDelete (id).then((task) => {
+    return res.send(task);
+  })
 });
 
+app.put('/complete-task/:id', (req, res) => {
+  // TODO: mark a task as complete
+  const id = req.params.id;
+  Task.findByIdAndUpdate (id, {isComplete:true}).then((task) => {
+    return res.send(task);
+  })
+});
+
+
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
-
-
 
